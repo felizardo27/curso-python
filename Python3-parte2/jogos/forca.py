62273ec63e8c51bebf8826f7c63eff6b2cf58d87
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-06-08 20:05:05 -0300

    update Python3-parte2

--- Python3-parte2/jogos/forca.py
+++ Python3-parte2/jogos/forca.py
@@ -4,7 +4,28 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
 
+    palavra = list()
+    for p in range(len(palavra_secreta)):
+        palavra.append("_")
+    
+    while not acertou and not  enforcou:
+        chute = input("Qual letra? ").strip(" ")
+        
+        for i, letra in enumerate(palavra_secreta):
+            if letra.upper() == chute.upper():
+                print(f"Encontrei a letra {letra} na posicao {i}")
+                palavra[i] = chute
+                
+        print("jogando...")
+            
+        for p in palavra:
+            print(p, end="")
+        print()
+            
     print("Fim de jogo!")
 
 if __name__ == "__main__":
