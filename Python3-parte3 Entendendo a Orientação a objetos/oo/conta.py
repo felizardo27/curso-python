85857cf2806fd3c5050aecb67b478196a47a12d1
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-06-15 09:40:44 -0300

    update Python3-parte3

--- Python3-parte3 Entendendo a Orientação a objetos/oo/conta.py
+++ Python3-parte3 Entendendo a Orientação a objetos/oo/conta.py
@@ -3,7 +3,20 @@ class Conta:
     
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo objeto... {self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        
+    def extrato(self):
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
+        
+    def deposita(self, valor):
+        self.__saldo += valor
+        
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
