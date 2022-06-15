5aa50f499b36c061f0ccb3a90a910447a88420a1
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-06-15 13:45:17 -0300

    update Python3-parte3

--- Python3-parte3 Entendendo a Orientação a objetos/oo/conta.py
+++ Python3-parte3 Entendendo a Orientação a objetos/oo/conta.py
@@ -1,4 +1,7 @@
 
+from re import L
+
+
 class Conta:
     
     def __init__(self, numero, titular, saldo, limite):
@@ -13,10 +16,40 @@ class Conta:
         
     def deposita(self, valor):
         self.__saldo += valor
+       
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = (self.__saldo + self.__limite)
+        return valor_a_sacar <= valor_disponivel_a_sacar
         
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f"O valor {valor} passou o limite.")
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+        
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+        
+    @staticmethod
+    def codigo_banco():
+        return "001"
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
