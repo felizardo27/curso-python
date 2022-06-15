bbc5007015c884d19925992e74dfee9b7681b0aa
Joao Pedro Felizardo <jp.felizardo27@gmail.com>
2022-06-15 18:28:44 -0300

    update Python3-parte4

--- Python3-parte4 avançando na orientação a objetos/modelo.py
+++ Python3-parte4 avançando na orientação a objetos/modelo.py
@@ -1,57 +1,49 @@
 
-class Filme:
+
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome.title()
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome.title()
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
+        self.temporada = temporadas
+        self._likes = 0
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-vingadores.dar_like()
-vingadores.dar_like()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} minutos - Likes {vingadores.likes}')
 
 
 friends = Serie('friends', 1994, 10)
-friends.dar_like()
+friends.dar_likes()
 friends.nome = 'amigos'
-print(f'Nome: {friends.nome} - Ano: {friends.ano} - Temporadas: {friends.temporadas} - Likes {friends.likes}')
+print(f'Nome: {friends.nome} - Ano: {friends.ano} - Temporadas: {friends.temporada} - Likes {friends.likes}')
