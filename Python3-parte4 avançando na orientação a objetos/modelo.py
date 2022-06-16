2fbde19244196641f4148593c8d40c167d362edd
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-06-16 10:21:16 -0300

    update Python3-parte4

--- Python3-parte4 avançando na orientação a objetos/modelo.py
+++ Python3-parte4 avançando na orientação a objetos/modelo.py
@@ -1,5 +1,4 @@
 
-
 class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
@@ -23,17 +22,13 @@ class Programa:
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self._likes = 0
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporada = temporadas
-        self._likes = 0
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
