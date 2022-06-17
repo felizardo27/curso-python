400090cc1757eca2b9d269a4e790d78bb8115928
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-06-17 16:22:45 -0300

    update Python3-parte4

--- Python3-parte4 avançando na orientação a objetos/modelo.py
+++ Python3-parte4 avançando na orientação a objetos/modelo.py
@@ -39,18 +39,44 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes' 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+        
+    def __getitem__(self, item):
+        return self._programas[item]
+        
+    @property
+    def listagem(self):
+        return self._programas
+    
+    def __len__(self):
+        return len(self._programas)
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
-
 friends = Serie('friends', 1994, 10)
+himym = Serie('How I met your mother', 2005, 9)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 friends.dar_likes()
 friends.dar_likes()
+himym.dar_likes()
+himym.dar_likes()
+tmep.dar_likes()
 
 
-filmes_e_series = [vingadores, friends]
+filmes_e_series = [vingadores, friends, himym, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho da Playlist: {len(playlist_fim_de_semana)}')
+
+
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
